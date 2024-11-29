02347b6b64b83e60f9979ad017613840b4973dae
Tomás Cid <tomasshadow97@gmail.com>
2024-11-29 18:24:10 -0300

    actualización views y gestión estado de mesas
    se agregaron views y serializers para los modelos faltantes (empleado, inventario, usuario y detallepedido). además se implementó la clase ActualizarEstadoMesaView para gestionar estado de las mesas.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # gestion-de-restaurante
 sistema de gestión de restaurantes con fines de aprendizaje academico.
 guia de instalación:
+instalar google cloud sdk
+gcloud components install cloud_sql_proxy
+
+
+instalar python
 crear entorno virtual de django para el backend
+instalar librerías
+
+configurar base de datos google cloud
+añadir ip restaurante dentro de las redes permitidas
+
---
+++ backend/api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PedidoViewSet, MesaViewSet, EmpleadoViewSet, InventarioViewSet, DetallePedidoViewSet, UsuarioViewSet, MyTokenObtainPairView, MyTokenRefreshView, ActualizarEstadoMesaView
+
+#routers
+router = DefaultRouter()
+router.register(r'pedidos', PedidoViewSet)
+router.register(r'mesas', MesaViewSet)
+router.register(r'empleados', EmpleadoViewSet)
+router.register(r'inventario', InventarioViewSet)
+router.register(r'detalles_pedidos', DetallePedidoViewSet)
+router.register(r'usuarios', UsuarioViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),  # Incluye las rutas creadas por el router
+    #otras rutas
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+    path('mesas/<int:mesa_id>/actualizar-estado/', ActualizarEstadoMesaView.as_view(), name='actualizar_estado_mesa'), #ruta de actualización manual de estado de mesa
+
+]
+
+#urlpatterns = [
+    # Otras URLs
+ #   path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+  #  path('api/token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+#]
